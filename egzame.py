3c6831bf355579d21062d374c76e2646e3afd5f5
azamjon <azamsaidov077@gmail.com>
2025-09-25 03:47:13 +0500

    contact-sms  manager project add

---
+++ egzame.py
@@ -0,0 +1,11 @@
+class Student:
+    def __init__(self, name, phone):
+        self.__name = name
+        self.__phone = phone
+
+    def get_age(self):
+        return self.__age
+   def set_age(self, age):
+       self.__age = age
+
+
